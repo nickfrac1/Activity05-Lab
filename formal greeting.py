52381e628c508be478fa225b5c349112e0a4a995
Nicholas Ibarra <nicholasbarra86@gmail.com>
2023-03-13 20:26:45 -0700

    Added a new function to the python script formal greeting.py

--- formal greeting.py
+++ formal greeting.py
@@ -2,3 +2,6 @@
 def test_function():
 	your_name = input("What is your name? ")
 	print(f"Hello {your_name}!")
+	weather = input("lovely weather today isn't it? Y/N"  )
+	print("Well I must be off then, Goodbye!")
+test_function():
